0ce637afef5efe80b9f03e8c89f7c39d56bb1b00
MrMModder <35263058+MrMModder@users.noreply.github.com>
2021-03-10 19:50:24 +0100

    aa
    jippie blonkies doen brr

---
+++ Assets/Scripts/heenenweer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heenenweer : MonoBehaviour
+{
+    public float speed = 1.19f;
+    Vector3 pointA;
+    Vector3 pointB;
+    public Vector3 cubePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubePos = transform.position;
+        pointA = new Vector3(7, 0, 3); //Begincoordinaten
+        pointB = new Vector3(cubePos.x - 1, cubePos.y, cubePos.z - 1); //eincoordinaten
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(cubePos, pointB, time);
+    }
+}
