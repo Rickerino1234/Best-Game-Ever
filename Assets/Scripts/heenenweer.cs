2b87ffedfcee088458e64d4eb4a7637378c5ead8
MrMModder <35263058+MrMModder@users.noreply.github.com>
2021-03-12 16:44:11 +0100

    script ongelofelijk hard geupgrade

--- Assets/Scripts/heenenweer.cs
+++ Assets/Scripts/heenenweer.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 public class heenenweer : MonoBehaviour
 {
     public float speed = 1.19f;
-    Vector3 pointA;
+    public Vector3 transformatie;
     Vector3 pointB;
-    public Vector3 cubePos;
+    Vector3 cubePos;
 
     // Start is called before the first frame update
     void Start()
     {
         cubePos = transform.position;
-        pointA = new Vector3(7, 0, 3); //Begincoordinaten
-        pointB = new Vector3(cubePos.x - 1, cubePos.y, cubePos.z - 1); //eincoordinaten
-        
+        pointB = new Vector3(cubePos.x + transformatie.x, cubePos.y, cubePos.z + transformatie.z); 
     }
 
     // Update is called once per frame
