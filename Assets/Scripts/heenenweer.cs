326ff78b6004684dbc78e2c4aa822a6da55596be
MrMModder <maarten@maarten.maarten>
2021-03-17 14:02:43 +0100

    gefixt
    gefixt

--- Assets/Scripts/heenenweer.cs
+++ Assets/Scripts/heenenweer.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,30 +24,3 @@ public class heenenweer : MonoBehaviour
         transform.position = Vector3.Lerp(cubePos, pointB, time);
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class heenenweer : MonoBehaviour
-{
-    public float speed = 1.19f;
-    public Vector3 transformatie;
-    Vector3 pointB;
-    Vector3 cubePos;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        cubePos = transform.position;
-        pointB = new Vector3(cubePos.x + transformatie.x, cubePos.y, cubePos.z + transformatie.z); 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float time = Mathf.PingPong(Time.time * speed, 1);
-        transform.position = Vector3.Lerp(cubePos, pointB, time);
-    }
-}
->>>>>>> 40dac03e5458784c0cec39b02fa2c35634e319c8
