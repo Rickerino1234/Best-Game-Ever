40b150a2e22a3478578bf19daf383ec9fc6cecfc
MrMModder <maarten@maarten.maarten>
2021-03-17 13:54:37 +0100

    test
    test

---
+++ Assets/Scripts/Sceneswitch2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneswitch2 : MonoBehaviour
+{
+    private int count;
+    void start()
+    {
+        count = 0;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+
+        if (count >= 5)
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
---
+++ Assets/Scripts/heenenweer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heenenweer : MonoBehaviour
+{
+    public float speed = 1.19f;
+    public Vector3 transformatie;
+    Vector3 pointB;
+    Vector3 cubePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubePos = transform.position;
+        pointB = new Vector3(cubePos.x + transformatie.x, cubePos.y, cubePos.z + transformatie.z); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(cubePos, pointB, time);
+    }
+}
